f2343ef56334dedeb58d91d03e374ce8c2c7d03e
carnasci <cen.master24@gmail.com>
2025-01-13 17:09:35 -0600

    Day 25 - manipulated data in csv file

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-with open("./weather_data.csv") as data_file:
-    data_list = data_file.readlines()
+# with open("./weather_data.csv") as data_file:
+#     data_list = data_file.readlines()
+#
+# print(data_list)
 
-print(data_list)
+import csv
+
+with open("./weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)
